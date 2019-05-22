08e32c99be7320060fd930803908ef249aae363c
Your Name <you@example.com>
2019-05-22 23:34:19 +0200

    chore: moving index.html to root project

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Main() {
     return(
         <div classname="name">
-            <section class="hero is-medium is-primary is-bold">
+            <section class="hero is-fullheight-with-navbar is-primary is-bold">
             <div class="hero-body">
             <div class="container">
                 <h1 class="title">Kristian Saenz</h1>
