0afcf8885ed465a5b0d9b9d05b8744d28f0c7b71
Your Name <you@example.com>
2019-05-22 23:51:26 +0200

    Updates

---
+++ precache-manifest.43230a3d29a188c8d68f8348e1138ed8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "46673e32db715b3500e8",
+    "url": "kristiansaenz.github.io/static/css/main.029a8e1e.chunk.css"
+  },
+  {
+    "revision": "46673e32db715b3500e8",
+    "url": "kristiansaenz.github.io/static/js/main.46673e32.chunk.js"
+  },
+  {
+    "revision": "ff625c58dbc7314fc1cc",
+    "url": "kristiansaenz.github.io/static/js/runtime~main.ff625c58.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "kristiansaenz.github.io/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "kristiansaenz.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e6f0d8c8375bf99cdbbb7c233a1a33a5",
+    "url": "kristiansaenz.github.io/index.html"
+  }
+];
